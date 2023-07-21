6ec2cd2e64a141496bbcba1b2138635953e47ed8
OliverDantasLeo <140180344+OliverDantasLeo@users.noreply.github.com>
2023-07-21 11:44:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-primeiroteste-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5umaop)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appH1 = document.getElementById('app-header');
+appH1.innerHTML = `JS Starter`;
+
+const appH2 = document.getElementById('app-page');
+appH2.innerHTML = `Primeiro App JS`;
